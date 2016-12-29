0107456e26e61038528fc0228136ff099ae8899f
kenpeter <figo2476@hotmail.com>
2016-12-29 11:55:09 +1100

    update

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,4 @@
+/*
 import React from "react";
 import {render} from "react-dom";
 
@@ -29,3 +30,85 @@ class App extends React.Component {
 }
 
 render(<App />, window.document.getElementById('app'));
+*/
+
+// import
+// create store
+// from
+// redux
+import { createStore } from "redux";
+
+// what reducer does is
+// get state and action -> state
+// const
+// reducer
+// (state, action)
+// =>
+// {}
+const reducer = (state, action) => {
+  // switch
+  // action.type
+  switch(action.type) {
+    // add
+    // state == 1
+    // state = 1 + 10 (payload)
+    case "ADD":
+      state = state + action.payload;
+      break;
+    
+    // substract
+    case "SUBSTRACT":
+      state = state - action.payload;
+      break;
+  }
+
+  // return new state
+  return state;
+}
+
+
+// const
+// store
+// create store
+// reducer
+// state: 1
+const store = createStore(reducer, 0);
+
+
+// because it is sub, so every time, there is action
+// it will react
+// store
+// subscribe
+// () => {}
+// console.log(x, y);
+// store
+// .getState();
+store.subscribe(() => {
+  console.log("store updated!", store.getState());
+});
+
+
+// store
+// dispatch
+// type: add
+// payload: 10
+store.dispatch({
+  type: "ADD",
+  payload: 10
+});
+
+store.dispatch({
+  type: "ADD",
+  payload: 10
+});
+
+store.dispatch({
+  type: "ADD",
+  payload: 10
+});
+
+store.dispatch({
+  type: "SUBSTRACT",
+  payload: 10
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,71 @@
+// path, require
+// path
 var path = require("path");
 
+// dist dir
+// path
+// resolve
+// __dirname, current dir, no "/"
+// dist, next to src
 var DIST_DIR = path.resolve(__dirname, "dist");
+
+// src dir
+// path
+// resolve
+// __dirname, current dir, no "/"
+// src
 var SRC_DIR = path.resolve(__dirname, "src");
 
+// var
+// config
 var config = {
-    entry: SRC_DIR + "/app/index.js",
-    output: {
-        path: DIST_DIR + "/app",
-        filename: "bundle.js",
-        publicPath: "/app/"
+  // entry point
+  // src_dir
+  // /app
+  // /index.js
+  entry: SRC_DIR + "/app/index.js",
+  
+  // output
+  output: {
+    // output path
+    // dist dir, next to src
+    // __dirname/dist/app/bundle.js
+    path: DIST_DIR + "/app",
+    // filename bundle.js
+    filename: "bundle.js",
+    // public path
+    // /app/
+    publicPath: "/app/"
     },
+    
+    // module
     module: {
-        loaders: [
-            {
-                test: /\.js?/,
-                include: SRC_DIR,
-                loader: "babel-loader",
-                query: {
-                    presets: ["react", "es2015", "stage-2"]
-                }
-            }
-        ]
+      // loaders
+      // array
+      loaders: [
+        {
+          // test
+          // //
+          // \.js?
+          test: /\.js?/,
+          // include src_dir
+          include: SRC_DIR,
+          // babel loader
+          loader: "babel-loader",
+          // query
+          query: {
+            // preset
+            // react
+            // es2015
+            // stage-2
+            // https://tc39.github.io/process-document/, lower number powerful
+            presets: ["react", "es2015", "stage-2"]
+          }
+        }
+      ]
     }
 };
 
+// module exports
+// config
 module.exports = config;
